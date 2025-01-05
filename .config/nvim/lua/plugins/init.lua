d462c4de4a36d47f403f6ee35cb2f5ae576286b2
arctair <arctair>
2025-01-05 21:00:47 +0000

    rustaceanvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -13,13 +13,19 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+      },
+    },
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5',
+  }
 }
